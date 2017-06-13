1a5dea3b53e2345a24fffd52f9ffe8bd9cf3e27d
Seth Watson <seth.watson1@gmail.com>
2017-06-13 13:17:20 -0600

    Added API, needs user input built

--- public/components/services/service.js
+++ public/components/services/service.js
@@ -1,4 +1,9 @@
 angular.module("contactRepsApp");
-angular.service("someService", ["$http", function($http)
+app.service("repsApiReq", ["$http", function ($http){
 
-]);
+    this.getRepInfo = function (repsInfo) {
+        return $http.get("https://www.googleapis.com/civicinfo/v2/representatives?address= + userinput + &key=AIzaSyCnS7XgZcVD8IfLIJlOZiuysz7S8p8BLcE");
+
+    };
+
+}]); 
